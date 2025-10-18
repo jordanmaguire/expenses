3c2470790da34879d0908f27667b9f039922fa4e
Jordan Maguire <a.jordan.maguire@gmail.com>
2025-10-18 08:06:31 +0000

    Transaction::Category can write fragments to file

--- category.rb
+++ category.rb
@@ -3,31 +3,43 @@ class Transaction::Category
     @all ||= []
   end
 
-  def self.from_file(category_name:, filename:)
-    string_fragments = File.readlines("categories/#{ filename }").map(&:strip)
-    add(name: category_name, string_fragments: string_fragments)
-  end
-
-  def self.add(name:, string_fragments:)
+  def self.add(name:, filename:)
     all << new(
       name: name,
-      string_fragments: string_fragments,
+      filename: filename
     )
   end
 
-  attr_accessor :name, :string_fragments
+  attr_accessor :name, :filename
 
-  def initialize(name:, string_fragments:)
+  def initialize(name:, filename:)
+    @filename = filename
     @name = name
-    @string_fragments = string_fragments
   end
 
   def matches?(transaction)
     string_fragments.any? { |string| transaction.narration.downcase.include?(string.downcase) }
   end
 
+  def string_fragments
+    @string_fragments ||= File.readlines(file_path).map(&:strip)
+  end
+
+  def add_string_fragment(string_fragment)
+    # Ensure the values are alphabetical for readability - use downcase because upper/lower do not have the same
+    # precedence.
+    @string_fragments = (string_fragments + [string_fragment]).sort_by(&:downcase)
+
+    # Write the sorted values to the file.
+    File.open(file_path, mode: "w") { |file| file.write(@string_fragments.join("\n")) }
+  end
+
   def to_s
     name
+  end
+
+  private def file_path
+    "categories/#{ filename }"
   end
 end
 
@@ -35,14 +47,16 @@ end
   "Cars",
   "Fast Food",
   "Groceries",
+  "Holidays",
   "Home Improvement",
   "Leisure",
   "Medical",
   "Pets",
+  "Recurring",
   "Shopping",
 ].each do |category_name|
-  Transaction::Category.from_file(
-    category_name: category_name,
+  Transaction::Category.add(
+    name: category_name,
     filename: category_name.gsub(" ", "_"),
   )
 end
