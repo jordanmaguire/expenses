6359bd12b6411f4e11e48643d8018f3856994e96
Jordan Maguire <a.jordan.maguire@gmail.com>
2025-10-20 03:33:53 +0000

    Expense accounts

---
+++ expense/account.rb
@@ -0,0 +1,28 @@
+class Expense::Account
+  def initialize(monthly_summaries:, file_path:)
+    @file_path = file_path
+    @monthly_summaries = monthly_summaries
+  end
+
+  def extract_expenses
+    CSV.parse(File.read(@file_path), headers: true) do |csv|
+      # This will be on credit cards for pending transactions
+      next if csv["Narration"].include?("AUTHORISATION ONLY")
+      # I pay amount into these accounts to pay off debts - do not include these transactions
+      next if csv["Debit"] == nil
+
+      transaction_date = Date.parse(csv["Transaction Date"])
+      transaction = Expense::Transaction.new(
+        amount: csv["Debit"],
+        date: transaction_date,
+        narration: csv["Narration"],
+      )
+
+      if transaction.category.nil?
+        Expense::TransactionCategoryPrompt.new(transaction: transaction).show_prompt
+      end
+
+      @monthly_summaries[transaction_date.month].transactions << transaction
+    end
+  end
+end
--- expenses.rb
+++ expenses.rb
@@ -8,6 +8,7 @@ require 'date'
 class Expense
 end
 
+require_relative './expense/account'
 require_relative './expense/transaction'
 require_relative './expense/category'
 require_relative './expense/transaction_category_prompt'
@@ -21,33 +22,13 @@ end
 monthly_summaries = MonthlySummary.for_current_year
 
 # BSB Number,Account Number,Transaction Date,Narration,Cheque Number,Debit,Credit,Balance,Transaction Type
-test_file_path = "/workspaces/expenses/sample/credit_card.csv"
-transactions = []
-CSV.parse(File.read(test_file_path), headers: true) do |csv|
-  next if csv["Narration"].include?("AUTHORISATION ONLY")
-  # I pay amount into this account to pay off debts - do not include these transactions
-  next if csv["Debit"] == nil
+expense_accounts = [
+  Expense::Account.new(monthly_summaries: monthly_summaries, file_path: "/workspaces/expenses/sample/credit_card.csv"),
+  Expense::Account.new(monthly_summaries: monthly_summaries, file_path: "/workspaces/expenses/sample/recurring_expenses.csv"),
+]
 
-  transaction_date = Date.parse(csv["Transaction Date"])
-
-  monthly_summaries[transaction_date.month].transactions << Expense::Transaction.new(
-    amount: csv["Debit"],
-    date: transaction_date,
-    narration: csv["Narration"],
-  )
-end
+expense_accounts.each(&:extract_expenses)
 
 monthly_summaries.values.reverse.each { _1.print_summary }
-
-# puts "#{ transactions.select(&:category).count } Transactions with a category"
-# puts "#{ transactions.reject(&:category).count } Transactions without a category"
-
-# transactions.reject(&:category).each do |transaction|
-#   # The transaction may be able to be categories based on a selection earlier. So we attempt to re-categorise first
-#   # and only prompt to choose a category if it still can't be found.
-#   if transaction.category.nil?
-#     Expense::TransactionCategoryPrompt.new(transaction: transaction).show_prompt
-#   end
-# end
 
 SpreadsheetGenerator.new(monthly_summaries.values).to_spreadsheet
